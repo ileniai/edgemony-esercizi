8e9989daf893fddcf26a1ef2e02b925f50fd5bc9
ileniai <118599605+ileniai@users.noreply.github.com>
2022-12-20 16:23:37 +0100

    20-12-2022

---
+++ 20-12-2022/script.js
@@ -0,0 +1,8 @@
+//arrey vuoto
+let foto = [];
+
+//utilizzare il ciclo for classico
+for (let i = 0; i < 10; i++){
+    foto.push(prompt ('Ciao! Iserisci una parola chiave che ti ricorda la fotografia'));
+}
+console.log (foto);
