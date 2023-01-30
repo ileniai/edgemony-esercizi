1d984435ccba589e15dc7783119ad2727054b394
Vincenzo Ingrassia <Vincy@MBP-di-Vincenzo.homenet.telecomitalia.it>
2023-01-30 18:45:12 +0100

    esercizio node 30-01-2023

--- 27-01-2023/api.js
+++ 27-01-2023/api.js
@@ -1,8 +1,6 @@
 const API_KEY = "b130743094e2ec9986e5b10108a7cabe";
 const BASE_URL = "https://api.themoviedb.org/3/";
 
-
-
 //https://api.themoviedb.org/3/tv/550seasons/1?api_key=<<api_key>>&language=en-US
 
 const GET = async(type = "tv", resource = "550") => {
---
+++ 30-01-2023/app.js
@@ -0,0 +1,27 @@
+//metodo require() => richiamo il modulo built-in
+
+const http = require(`http`);//creato oggetto
+
+//crea percorsi server
+const server = http.createServer((req, res) => {
+
+//imposto i percorsi
+
+    if(req.url === `/`){
+        res.end(`Home`)
+    }
+    
+   else if(req.url === `/studenti`)
+    res.end(`pagina studenti`)
+   
+
+   else if(req.url === `/corsi`)
+    res.end(`pagina dedicata ai corsi`)
+
+
+    else{
+        //codice di errore per risorsa non trovato
+        res.writeHead(404)
+        res.end(`pagina non trovata`)
+    }
+})
