daf0a5c4dca8326a75b9354f7cf1d53b80617d12
Vincenzo Ingrassia <Vincy@MBP-di-Vincenzo.homenet.telecomitalia.it>
2023-02-24 13:11:26 +0100

    esercizio quasi completo

--- codeweek/script.js
+++ codeweek/script.js
@@ -6,19 +6,14 @@ const qS = (element) => document.querySelector(element);
 
 //const bodyEl = qS('.body')
 const containerEl = qS('#container_user')
-const btnEl = qS('.letter')
+const btnAe = qS('#ae')
+const btnFl = qS('#fl')
+const btnMz = qS('#mz')
+const btnEl = qS('#all')
+const searchEl = qS('#search-letter')
 
-
-
-GET('users').then((users)=>
-    users.forEach((user)=>{
-       createCard(user)
-    })
-)
-
-
-const createCard = (user)=>{
-    const{ id, name, username, email, phone} = user
+const createCard = (user, filter)=>{
+    const{id, name, username, email, phone} = user
 
     const cardEl = cE('div')
     cardEl.className = 'card'
@@ -47,38 +42,130 @@ const createCard = (user)=>{
     phoneEl.textContent = phone
     phoneEl.className = 'info'
 
+
     boxEl.append(nameEL, userNameEl, emailEl, phoneEl);
     cardEl.append(imgEl, boxEl);
-    containerEl.appendChild(cardEl)
-   
-}
+    //containerEl.appendChild(cardEl)
 
-/*btnEl.addEventListener('click',(e)=>{
-    function filterUsersByLetter(letter) {
-        const filteredUsers = users.filter(user => user.name.startsWith(letter));
-        if (filteredUsers.length === 0) {
-          document.getElementsByClassName('card').innerHTML = 'Nessun account trovato per questo filtro';
-        } else {
-          const cards = filteredUsers.map(user => createCard(user));
-          document.getElementsByClassName('card').innerHTML = '';
-          cards.forEach(card => document.getElementsByClassName('card').appendChild(card));
-        }
+
+  const initial = name[0]
+    if(filter == 'ae'){
+      if(initial.toLowerCase() == 'a' 
+      || initial.toLowerCase() == 'b'
+      || initial.toLowerCase() == 'c'
+      || initial.toLowerCase() == 'd'
+      || initial.toLowerCase() == 'e'){
+        containerEl.appendChild(cardEl)
       }
+    }else{'Nessun utente trovato'}
+    if(filter == 'fl'){
+      if(initial.toLowerCase() == 'f'
+      || initial.toLowerCase() == 'g'
+      || initial.toLowerCase() == 'h'
+      || initial.toLowerCase() == 'i'
+      || initial.toLowerCase() == 'k'
+      || initial.toLowerCase() == 'l'){
+        containerEl.appendChild(cardEl)
+      }
+    }else{'Nessun utente trovato'}
+    if(filter == 'mz'){
+      if(initial.toLowerCase() == 'm'
+      || initial.toLowerCase() == 'n'
+      || initial.toLowerCase() == 'o'
+      || initial.toLowerCase() == 'p'
+      || initial.toLowerCase() == 'r'
+      || initial.toLowerCase() == 's'
+      || initial.toLowerCase() == 't'
+      || initial.toLowerCase() == 'v'
+      || initial.toLowerCase() == 'z'){
+        containerEl.appendChild(cardEl)
+      }
+    }else{'Nessun utente trovato'}{
+    }if(filter == 'all'){
+      containerEl.appendChild(cardEl)
+   
+    }
     
+  }
+
+btnAe.addEventListener("click",()=>{
+  containerEl.innerHTML = ''
+  GET('users').then((users)=>
+  users.forEach((user)=>{
+     createCard(user, 'ae')
+  }))
+});
+btnFl.addEventListener("click", () => {
+  containerEl.innerHTML = ''
+  GET('users').then((users)=>
+  users.forEach((user)=>{
+     createCard(user, 'fl')
+  }))
+});
+btnMz.addEventListener("click", () => {
+  containerEl.innerHTML = ''
+  GET('users').then((users)=>
+  users.forEach((user)=>{
+     createCard(user, 'mz')
+  }))
+});
+
+btnEl.addEventListener("click", () => {
+  containerEl.innerHTML = ''
+  GET('users').then((users)=>
+  users.forEach((user)=>{
+     createCard(user, 'all')
+  }))
+  
+});
+
+
+
+/*const onUsersDelete = () => {
+  const usersEls = document.querySelectorAll('.card')
+  usersEls.forEach((user) => user.remove())
+}
+searchEl.addEventListener('search', (e) => {
+  onUsersDelete()
+  let searchedValue = e.target.value
+  searchedValue = searchedValue.charAt(0).toUpperCase() + searchedValue.slice(1)
+  GET('users').then((users) => {
+    users.filter((user) => {
+      if (
+        user.name.includes(searchedValue) ||
+        user.username.includes(searchedValue)
+      
+      ) {/*createCard(user)*/
+    /*  }else{'Nessun utente trovato'}
+    })
+  })
 })*/
 
 
-/*const filteredUsers = "letter, A-E.";
-console.log(filteredUsers.startsWith("letter"));*/
-btnEl.addEventListener('click',()=>{
-  const users = checkUserByLetter ('A-E');
-  function checkUserByLetter(name){
-    const filteredUser = users.filter((user)=>{
-      return user.name === name;
+     let serchedName = '';
+     
+     const cardDelete = () =>{
+      const cardEl = document.querySelectorAll('.card')
+      cardEl.forEach((user) => user.remove());
+     };
+
+     searchEl.addEventListener('input',(event)=>{
+      cardDelete();
+      serchedName = event.target.value;
+      //console.log(serchedName);
+
+      GET('users').then((users)=>
+        users.map((user)=>{
+          if(user.name.includes(serchedName))
+          console.log(serchedName);
+          containerEl.append(createCard(user));
+        })
+      )
     })
-    return filteredUser;
-  }
-})
+  
 
 
 
+/*const pippo = cE('p')
+        pippo.textContent = user.name
+        containerEl.appendChild(pippo)*/
