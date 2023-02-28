c56aa33a177c8a1fb3713d02addce43f55fa748d
Vincenzo Ingrassia <Vincy@MBP-di-Vincenzo.homenet.telecomitalia.it>
2023-02-28 18:27:46 +0100

    primo esercizio di react

--- 28-02-2023/src/components/footer/Footer.jsx
+++ 28-02-2023/src/components/footer/Footer.jsx
@@ -4,11 +4,9 @@ const Footer = () => {
     return(
         <div className="Footer">
         <h4 className="titoli">Contatti ☎️</h4>
-        <hr className="hr_footer"/>
         <h4 className="titoli">Articoli recenti 📚</h4>
-        <hr className="hr_footer"/>
         <h4 className="titoli">Risorse necessarie 🔎</h4>
-        <hr className="hr_footer"/>
+        
         </div>
     )
 }
