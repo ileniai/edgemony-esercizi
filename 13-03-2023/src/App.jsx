8652ab3709ca8f929a1fee5df86b5e9e55cfd83c
Vincenzo Ingrassia <Vincy@MBP-di-Vincenzo.homenet.telecomitalia.it>
2023-03-13 18:21:14 +0100

    esercizio 13-03-2023 con bozza calendario

--- 13-03-2023/src/App.jsx
+++ 13-03-2023/src/App.jsx
@@ -1,25 +1,27 @@
-//import {useState} from "react"
+import {useState} from "react"
 import './App.css'
 import TodosList from './components/todosList/TodosList'
 import Day from "./components/day"
 
 function App() {
 
-  //const [activeElement, setActiveElement] = useState(0)
+  const [activeElement, setActiveElement] = useState(0)
   return (
     <div className="App">
-      {/*[...new Array(30)].map((item, i)=>(
+      <h2 className='App_title'>Todo List</h2>
+      <p className='App_text'>Clicca sul titolo "Todos" per aggiungere una cosa da fare 😉</p>
+      <TodosList />
+      <div className="title_calendar">Calendar</div>
+      <div className="App_calendar">
+      {[...new Array(30)].map((item, i)=>(
       <Day 
       content = {i+1}
       activeElement = {activeElement}
       setActiveElement = {setActiveElement}
       key={i}
       />
-      ))*/}
-      <h2 className='App_title'>Todo List</h2>
-      <p className='App_text'>Clicca sul titolo "Todos" per aggiungere una cosa da fare 😉</p>
-      
-      <TodosList />
+      ))}
+      </div>
       </div>
   )
 }
